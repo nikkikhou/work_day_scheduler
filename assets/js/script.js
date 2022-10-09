b589d1fcd7aeaa3c187f58d060ff331e2a5c28f7
nikkikhou <nikkikhou@gmail.com>
2022-10-09 16:59:10 -0400

    applying past, present, and future classes to current hour

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,11 +2,20 @@ var today = moment();
 $("#currentDay").text(today.format("LLLL"));
 
 for (let index = 9; index < 18; index++) {
-  // const element = array[index];
 
-// timeblocks 
-  var hourDivEl = $('<div class="row time-block"></div>');
-  var formatedTime = moment(index, "hh").format("LT")
+  // timeblocks
+  var rowClass = "";
+  if (today.hour() > index) {
+    rowClass = "past";
+  }
+  if (today.hour() === index) {
+    rowClass = "present";
+  }
+  if (today.hour() < index) {
+    rowClass = "future";
+  }
+  var hourDivEl = $(`<div class="row time-block ${rowClass}"></div>`);
+  var formatedTime = moment(index, "hh").format("LT");
   hourDivEl.append(`<div class="col-md-1 hour">${formatedTime}</div>`);
   hourDivEl.append('<textarea class="col-md-10 description"></textarea>');
   hourDivEl.append(
