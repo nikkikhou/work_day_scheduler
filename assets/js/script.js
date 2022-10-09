06773a1e836f27144b27af81f296fd380af7d779
nikkikhou <nikkikhou@gmail.com>
2022-10-09 16:34:14 -0400

    hours formated to 12 hr with moment js

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,8 +4,10 @@ $("#currentDay").text(today.format("LLLL"));
 for (let index = 9; index < 18; index++) {
   // const element = array[index];
 
+// timeblocks 
   var hourDivEl = $('<div class="row time-block"></div>');
-  hourDivEl.append(`<div class="col-md-1 hour">${index}</div>`);
+  var formatedTime = moment(index, "hh").format("LT")
+  hourDivEl.append(`<div class="col-md-1 hour">${formatedTime}</div>`);
   hourDivEl.append('<textarea class="col-md-10 description"></textarea>');
   hourDivEl.append(
     '<button class="btn saveBtn col-md-1"><i class="fas fa-save"></i></button>'
