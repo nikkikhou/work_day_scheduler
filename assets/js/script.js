a0161b41e858857db62cf9c7ba075cc5c58761c6
nikkikhou <nikkikhou@gmail.com>
2022-10-09 20:08:23 -0400

    when save button is clicked it is saved to local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,6 @@ var today = moment();
 $("#currentDay").text(today.format("LLLL"));
 
 for (let index = 9; index < 18; index++) {
-
   // timeblocks
   var rowClass = "";
   if (today.hour() > index) {
@@ -14,12 +13,29 @@ for (let index = 9; index < 18; index++) {
   if (today.hour() < index) {
     rowClass = "future";
   }
-  var hourDivEl = $(`<div class="row time-block ${rowClass}"></div>`);
+  var hourDivEl = $(
+    `<div id="hour-${index}" class="row time-block ${rowClass}"></div>`
+  );
+  var savedText = localStorage.getItem("hour-" + index)
+  if (!savedText) {
+    savedText = ""
+  }
   var formatedTime = moment(index, "hh").format("LT");
   hourDivEl.append(`<div class="col-md-1 hour">${formatedTime}</div>`);
-  hourDivEl.append('<textarea class="col-md-10 description"></textarea>');
   hourDivEl.append(
+    `<textarea class="col-md-10 description">${savedText}</textarea>`
+  );
+  var saveBtnEl = $(
     '<button class="btn saveBtn col-md-1"><i class="fas fa-save"></i></button>'
   );
+  hourDivEl.append(saveBtnEl);
+  saveBtnEl.click(function (event) {
+    var hourEl = $(this).parent();
+    var hourId = $(this).parent().attr("id");
+    var hourText = hourEl.find("textarea").val();
+    console.log(hourId);
+    console.log(hourText);
+    localStorage.setItem(hourId, hourText);
+  });
   $(".container").append(hourDivEl);
 }
